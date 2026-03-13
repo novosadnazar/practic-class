a81790ab0b6ab9343c2727e91141014e65bfd733
novosadnazar <163895331+novosadnazar@users.noreply.github.com>
2026-03-13 19:12:25 +0200

    fix

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+// import { render } from "@testing-library/react";
 import { Component } from "react";
 import style from"./Section.module.css"
 export default class Section extends Component {
